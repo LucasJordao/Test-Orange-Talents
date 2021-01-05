ec99952fa9b7bf7f0560f927862f22d99ab6b414
LucasJordao <lucaswill12@hotmail.com>
2021-01-05 16:58:57 -0300

    Instanciando 2 bancos quando a aplicação é iniciada

--- src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
+++ src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
@@ -1,13 +1,35 @@
 package com.lucas.testorangetalents;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TestorangetalentsApplication {
+import com.lucas.testorangetalents.domains.Banco;
+import com.lucas.testorangetalents.repositories.BancoRepository;
 
+@SpringBootApplication
+public class TestorangetalentsApplication implements CommandLineRunner{
+
+	// Repositories
+	@Autowired
+	private BancoRepository bancoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestorangetalentsApplication.class, args);
+	}
+	
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Banco b1 = new Banco(null, "Zup Banco");
+		Banco b2 = new Banco(null, "Orange Banco");
+		
+		bancoRepository.saveAll(Arrays.asList(b1, b2));
 	}
 
 }
