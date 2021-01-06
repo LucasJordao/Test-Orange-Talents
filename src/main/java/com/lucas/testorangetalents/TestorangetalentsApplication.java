fbf8df0c11270945d7e511326d2f2e44a2d5525f
LucasJordao <lucaswill12@hotmail.com>
2021-01-06 09:59:59 -0300

    Retirando a inicialização das entidades

--- src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
+++ src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
@@ -1,9 +1,5 @@
 package com.lucas.testorangetalents;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,31 +8,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.lucas.testorangetalents.domains.Banco;
-import com.lucas.testorangetalents.repositories.BancoRepository;
-
 @Configuration
 @SpringBootApplication
-public class TestorangetalentsApplication implements CommandLineRunner{
+public class TestorangetalentsApplication{
 
-	// Repositories
-	@Autowired
-	private BancoRepository bancoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TestorangetalentsApplication.class, args);
 	}
-	
-	
-	@Override
-	public void run(String... args) throws Exception{
-		
-		Banco b1 = new Banco(null, "Zup Banco");
-		Banco b2 = new Banco(null, "Orange Banco");
-		
-		bancoRepository.saveAll(Arrays.asList(b1, b2));
-	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
