285800696542281df0aaf400f06503e2ee51c8ec
LucasJordao <lucaswill12@hotmail.com>
2021-01-07 00:31:48 -0300

    Resolvendo problema de cors

--- src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
+++ src/main/java/com/lucas/testorangetalents/TestorangetalentsApplication.java
@@ -2,11 +2,7 @@ package com.lucas.testorangetalents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @SpringBootApplication
@@ -14,15 +10,6 @@ public class TestorangetalentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestorangetalentsApplication.class, args);
-	}
-	
-
-	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-		return source;
 	}
 
 }
---
+++ src/main/java/com/lucas/testorangetalents/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lucas.testorangetalents.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
